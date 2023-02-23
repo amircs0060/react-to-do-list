66c236e77526b843a1bcef7a17232e0bb16d2ab7
amircs0060 <80102762+amircs0060@users.noreply.github.com>
2023-02-23 22:44:17 +0530

    Improve css

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,9 +10,9 @@ const Todo = () => {
   return (
     <div className="main-div">
       <div className="child-div">
-        <figure>
-          <figcaption> Add Your List Here !!</figcaption>
-        </figure>
+        <span className="icon">
+          <i class="fa fa-list"></i>Add your todo list here !
+        </span>
         <div className="addItems">
           <span class="input-element">
             <span
