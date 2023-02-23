a1d06e2b2720acc8e5c69923dfe14c4c745516cb
amircs0060 <80102762+amircs0060@users.noreply.github.com>
2023-02-23 17:14:55 +0530

    Added some initial styling

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToDo, deleteToDo, deleteAllToDo } from '../actions/index';
+import './todo.css';
 
 const Todo = () => {
   const [inputValue, setInputValue] = useState('');
@@ -13,37 +14,50 @@ const Todo = () => {
           <figcaption> Add Your List Here !!</figcaption>
         </figure>
         <div className="addItems">
-          <input
-            type="text"
-            placeholder="Add Items.."
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <i
-            className="fa fa-plus add-btn"
-            title="Add Item"
-            onClick={() => dispatch(addToDo(inputValue), setInputValue(''))}
-          ></i>
+          <span class="input-element">
+            <span
+              className="fa fa-plus add-btn"
+              title="Add Item"
+              onClick={() => dispatch(addToDo(inputValue), setInputValue(''))}
+            ></span>
+            <input
+              type="text"
+              placeholder="Add Items.."
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          </span>
         </div>
         <div className="showItems">
           {list.map((elem) => {
             return (
               <div className="eachItems" key={elem.id}>
-                <h3>{elem.data}</h3>
+                {/* <h3>{elem.data}</h3>
                 <div className="todo-btn">
-                <i
+                  <i
+                    className="far fa-trash-alt add-btn"
+                    title="Delete Item"
+                    onClick={() => dispatch(deleteToDo(elem.id))}
+                  ></i>
+                </div> */}
+                <span
                   className="far fa-trash-alt add-btn"
                   title="Delete Item"
                   onClick={() => dispatch(deleteToDo(elem.id))}
-                ></i>
-                </div>
+                ></span>
+                <h3>{elem.data}</h3>
               </div>
             );
           })}
         </div>
-        <div className="showItems">
-          <button className="btn" onClick={() => dispatch(deleteAllToDo())}>Remove All</button>
-          </div>
+        <div className="remove-button">
+          <button
+            className="w3-btn w3-white w3-border w3-border-red w3-round-large"
+            onClick={() => dispatch(deleteAllToDo())}
+          >
+            Remove All
+          </button>
+        </div>
       </div>
     </div>
   );
